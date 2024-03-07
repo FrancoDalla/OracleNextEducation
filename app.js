bf47bcaff6dfe65b3c60776c0ca68b68e8e3ca8f
FrancoDalla <157550038+FrancoDalla@users.noreply.github.com>
2024-03-07 14:10:46 -0300

    Me olvide de hacer commits o.o

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 function testFunction(){
     alert('PALA');
 }
+
 function buttonTest(){
     alert('ANCHA');
+}
+
+function desencriptarTexto(){
+    alert('asi es, esto deberia mostrar el texto desencriptado');
+}
+
+function encriptarTexto(){
+    alert('Esto deberia mostrar el texto encriptado');
+
+}
+
+function mostrarEnPantalla(){
+    let message = document.getElementById("mensaje").value;
+    let img = document.getElementById('monigote');
+    img.style.visibility = 'hidden';
+    document.getElementById('textBox').className = 'cajaTextoChange';
+    let texto = document.getElementById('muestraTexto');
+    texto.className = "textoMuestra";
+    texto.innerHTML = message;
+    alert(message);
 }

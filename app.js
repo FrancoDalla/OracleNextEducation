5cafd19287cb2be55adbd06ab32cfc64e945b96c
FrancoDalla <157550038+FrancoDalla@users.noreply.github.com>
2024-03-20 13:37:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-function testFunction(){
-    alert('PALA');
-}
-
-function buttonTest(){
-    alert('ANCHA');
-}
-
 function desencriptarTexto(){
     alert('asi es, esto deberia mostrar el texto desencriptado');
 }
@@ -19,7 +11,7 @@ function crearBoton(divName){
     let auxDiv = document.getElementById(divName);
     let buttonCopy = document.createElement("BUTTON");
     let text = document.createTextNode("COPIAR");
-    buttonCopy.className="button copyButton";
+    buttonCopy.className="copyButton";
     buttonCopy.appendChild(text);
     buttonCopy.addEventListener("click", copyFunction);
     auxDiv.appendChild(buttonCopy);
@@ -35,26 +27,29 @@ function fufufu(){
 
 }
 
-function encrypt(text){
-    /*let reemplazo = {
-        a:'av',
-        e:'ev',
-        i:'iv',
-        o:'ov',
-        u:'uv'
-    };
-    var encryptText = text.replace(/[aeiou]/gi, reemplazo);*/
+function encrypt(){
+    let text = document.getElementById("mensaje").value;
     let encryptText = text
-        .replaceAll('a', 'av')
-        .replaceAll('e', 'ev')
-        .replaceAll('i', 'iv')
-        .replaceAll('o', 'ov')
-        .replaceAll('u', 'uv');
-    alert(encryptText);
+        .replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('a', 'ai')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat');
+    mostrarEnPantalla(encryptText);
 }
 
-function mostrarEnPantalla(){
-    let message = document.getElementById("mensaje").value;
+function desEncrypt(){
+    let text = document.getElementById("mensaje").value;
+    let desencryptText = text
+        .replaceAll('enter', 'e')
+        .replaceAll('imes', 'i')
+        .replaceAll('ai', 'a')
+        .replaceAll('ober', 'o')
+        .replaceAll('ufat','u');
+    mostrarEnPantalla(desencryptText);
+}
+
+function mostrarEnPantalla(message){
     let img = document.getElementById('monigote');
     img.style.visibility = 'hidden';
     document.getElementById('textBox').className = 'cajaTextoChange';
@@ -62,5 +57,4 @@ function mostrarEnPantalla(){
     let texto = document.getElementById('muestraTexto');
     texto.className = "textoMuestra";
     texto.innerHTML = message;    
-    alert(message);
 }
